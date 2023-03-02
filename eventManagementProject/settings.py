83c85d91290443d88e3d8644afb7fd29cdf96863
Aniket Pathak <aniketpathak34@gmail.com>
2023-03-02 11:02:29 +0530

    test4

--- eventManagementProject/settings.py
+++ eventManagementProject/settings.py
@@ -127,10 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'eventAdminApp/static/')
-]
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'eventAdminApp/static/')
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
