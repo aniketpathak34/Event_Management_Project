37cec34e92cd2e48ca6cc76188c670b0e870df8d
Aniket Pathak <aniketpathak34@gmail.com>
2023-03-02 10:50:02 +0530

    test3

--- eventManagementProject/urls.py
+++ eventManagementProject/urls.py
@@ -17,12 +17,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('eventAdminApp.urls')),
     path('', include('django.contrib.auth.urls')),
 ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
